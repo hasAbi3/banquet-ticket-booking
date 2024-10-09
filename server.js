35b0e4092d1ad0d72efe5ba6268157bcd0742f50
hasAbi3 <ag736s@missouristate.edu>
2024-10-09 11:11:54 -0500

    change port config

--- server.js
+++ server.js
@@ -99,7 +99,9 @@ app.post('/send-receipt', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server is runninng on port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
 
